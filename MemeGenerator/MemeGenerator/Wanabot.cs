9c60a849e2e700a064b9ab9423a512750d1d8694
maximechronoresto <maxime@chronoresto.com>
2015-04-29 17:31:49 +0200

    Top/bot text automatically filled when not set

--- MemeGenerator/MemeGenerator/Wanabot.cs
+++ MemeGenerator/MemeGenerator/Wanabot.cs
@@ -1,32 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MemeGenerator
 {
     public class WanaBot
     {
         private const string RequestMemePostUrl = "https://api.imgflip.com/caption_image";
-        private static string _selectedMeme = "";
+        const string Username = "lwpowa";
+        const string Password = "imgflippassword";
 
         private static void Main()
         {
-            Console.WriteLine("Select meme ID you wanna caption");
+            var selectedMeme = "";
+
+            Console.WriteLine("Select meme ID you wanna caption:\n");
+
+            var i = 1;
+
             foreach (var entry in MemeDictionnaryClass.MemeDictionnary)
             {
-                Console.WriteLine(entry.Key + " : " + entry.Value);
+                Console.WriteLine(entry.Key + " : " + i++);
             }
-            _selectedMeme = Console.ReadLine();
-            //if (!MemeDictionnaryClass.MemeDictionnary.ContainsValue(_selectedMeme))
+
+            selectedMeme = Console.ReadLine();
+
+            i = 1;
+            int result;
+
+            int.TryParse(selectedMeme, out result);
+            foreach (var entry in MemeDictionnaryClass.MemeDictionnary)
+            {
+                if (result == i)
+                    selectedMeme = entry.Value.ToString();
+                i++;
+            }
+
             Console.WriteLine("Enter top text");
             var topText = Console.ReadLine();
 
+            if (topText == "")
+            {
+                topText = MemeDictionnaryClass.MemeDictionnary
+                    .First(meme => meme.Value == Convert.ToInt32(selectedMeme)).Key;
+
+            }
             Console.WriteLine("Enter bottom text");
             var botText = Console.ReadLine();
 
-            const string username = "lwpowa";
-            const string password = "MAXIMEH";
+            if (botText == "")
+            {
+                botText = MemeDictionnaryClass.MemeDictionnary
+                    .First(meme => meme.Value == Convert.ToInt32(selectedMeme)).Key;
 
-            Console.WriteLine("SelectMeme : " + _selectedMeme + " top text : " + topText + " bot text = " + botText);
-            var url = WebClient.SendRequest(RequestMemePostUrl, _selectedMeme, username, password, topText, botText);
+            }
+
+            Console.WriteLine("SelectMeme : " + selectedMeme + " top text : " + topText + " bot text = " + botText);
+            var url = WebClient.SendRequest(RequestMemePostUrl, selectedMeme, Username, Password, topText, botText);
             Console.WriteLine("Meme generated: " + url);
             System.Diagnostics.Process.Start(url.ToString());
             Console.ReadLine();
